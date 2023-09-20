52555bf2114b51853783266d157beeca302b7f7d
Christian Ledesma <softcodify@gmail.com>
2023-09-20 11:33:44 -0300

    Se instaló dotenv

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const repoVar1 = process.env.REPO_VAR1;
 const envVar1 = process.env.ENV_VAR1;
 
+console.log(`Valor de REPO_VAR1: ${repoVar1}`);
+console.log(`Valor de ENV_VAR1: ${envVar1}`);
+
 const contenedor = document.querySelector(".contenedor")
 
 contenedor.innerHTML = `
