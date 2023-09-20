f6482a33597c796ac26f4a365236b82b638ed9e3
Christian Ledesma <softcodify@gmail.com>
2023-09-20 11:27:43 -0300

    Se muestran variables de entorno en el fornt

---
+++ index.js
@@ -0,0 +1,9 @@
+const repoVar1 = process.env.REPO_VAR1;
+const envVar1 = process.env.ENV_VAR1;
+
+const contenedor = document.querySelector(".contenedor")
+
+contenedor.innerHTML = `
+<p>Valor de REPO_VAR1: ${repoVar1}<p/>
+<p>Valor de ENV_VAR1: ${envVar1}<p/>
+`
